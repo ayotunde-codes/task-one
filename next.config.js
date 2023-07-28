a71368d05ec971eb37a3ebc2644af80aeac3275c
Ayotunde Obasa <55897771+ayotunde-codes@users.noreply.github.com>
2023-07-28 18:53:48 +0100

    chore: svgr set up

--- next.config.js
+++ next.config.js
@@ -3,7 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
   trailingSlash: true,
   distDir: "dist",
-  output: "export",
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/i,
