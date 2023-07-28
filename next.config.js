229c18bcc6ca2fccd928643e78a5a1a4cb4b023e
Ayotunde Obasa <55897771+ayotunde-codes@users.noreply.github.com>
2023-07-28 14:03:54 +0100

    svgr install

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  trailingSlash: true,
+  distDir: "dist",
+  output: "export",
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/i,
+      issuer: /\.[jt]sx?$/,
+      use: ["@svgr/webpack"],
+    });
+
+    return config;
+  },
 };
 
 module.exports = nextConfig;
